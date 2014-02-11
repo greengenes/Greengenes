a3c7341e25fe96bb45289a625c8e854198e75ae8
Daniel McDonald <mcdonadt@colorado.edu>
2014-02-11 13:37:17 -0700

    bug fixes on filter_met_chi

--- scripts/filter_met_chi.py
+++ scripts/filter_met_chi.py
@@ -5,22 +5,13 @@
 from sys import argv
 from cogent.parse.fasta import MinimalFastaParser
 
-__author__ = "Daniel McDonald"
-__copyright__ = "Copyright 2012, Greengenes"
-__credits__ = ["Daniel McDonald"]
-__license__ = "GPL"
-__version__ = "0.1-dev"
-__maintainer__ = "Daniel McDonald"
-__email__ = "mcdonadt@colorado.edu"
-__status__ = "Development"
-
 if __name__ == '__main__':
 
     metric_lines = [l.strip().split('\t') for l in open(argv[1]) if not l.startswith('#')]
     chi_lines = [l.strip().split('\t') for l in open(argv[2]) if not l.startswith('#')]
 
     chi = set([r[0] for r in chi_lines])
-    metrics = dict([(id_, (inv, nonacgt)) \
+    metrics = dict([(id_, (float(inv), float(nonacgt))) \
                         for id_,inv,nonacgt in metric_lines])
 
     seqs = MinimalFastaParser(open(argv[3]))
@@ -34,13 +25,14 @@ if __name__ == '__main__':
             continue
 
         inv, nonacgt = metrics[i]
-        print i, inv, nonacgt
-
+        
         if inv < 0.9:
-            output_reasons.write("%s\tinvariants < 90%\n" % i)
+            output_reasons.write(i + "\tinvariants < 90%")
+            output_reasons.write("\n")
             continue
         if nonacgt > 0.01:
-            output_reasons.write("%s\tnon ACGT > 1%\n" % i)
+            output_reasons.write(i + "\tnon ACGT > 1%")
+            output_reasons.write("\n")
             continue
         
         output_seqs.write(">%s\n%s\n" % (i,s))
